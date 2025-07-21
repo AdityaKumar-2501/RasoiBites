7af17039661389501ee7b778c5ddc0e5fe7f6246
Aditya <adityakashyap2501@gmail.com>
2025-07-21 21:04:11 +0530

    Hero done need to work more on ui

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,11 +1,35 @@
 import Container from "../Container";
 import NavBar from "../ui/NavBar";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="bg-bites-green h-[100vh]">
+    <div className="bg-concentric h-screen">
       <Container>
         <NavBar />
+        <main className="grid grid-cols-1 md:grid-cols-2 gap-10 min-h-screen items-center">
+          {/* Left: Tagline */}
+          <aside className="flex flex-col justify-center items-start gap-10 text-white p-4">
+            <h1 className="hero-heading outlined-text text-[8rem] text-base/24">
+              Snack <br /> With No <br /> Limits
+            </h1>
+            <p className="body-text text-lg max-w-xl">
+              When you&apos;re on the go and don&apos;t have time to cook, snacks are the
+              perfect solution. Always be prepared for the mid-day hunger.
+            </p>
+          </aside>
+
+          {/* Right: Product Image */}
+          <div className="flex justify-center items-center">
+            <Image
+              src="/hero-img.jpg"
+              width={300}
+              height={400}
+              alt="Hero Image"
+              className="rotate-15 floating-animation translate-x-6 translate-y-2 "
+            />
+          </div>
+        </main>
       </Container>
     </div>
   );
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -2,12 +2,12 @@ import { NavLinks } from "@/constants";
 
 const NavBar = () => {
   return (
-    <div className="flex-center pt-8 gap-[40rem]">
-      <a className="hero-heading text-2xl cursor-pointer" href="#">
+    <div className="flex justify-between pt-8 gap-[40rem]">
+      <a className="hero-heading text-3xl cursor-pointer" href="#">
         RasoiBites
       </a>
 
-      <ul className="flex-center gap-8 text-xl body-text">
+      <ul className="flex-center gap-20 text-xl body-text">
         {NavLinks.map(nav => (
           <li key={nav.id}>
             <a href={nav.link}>{nav.name}</a>
