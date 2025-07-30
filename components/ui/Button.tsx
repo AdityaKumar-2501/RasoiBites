06b4245db2d4bbed62571ed69722e43fb73fde8f
Aditya <adityakashyap2501@gmail.com>
2025-07-30 15:02:10 +0530

    resolve button error

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,13 +1,22 @@
-
 type Props = {
-  text: string,
-  className?: string,
-}
+  text: string;
+  className?: string;
+  onClick?: () => void;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+};
 
-const Button = ({text, className}: Props) => {
+const Button = ({ text, className = '', onClick, onMouseEnter, onMouseLeave }: Props) => {
   return (
-    <div className={`${className} cursor-pointer px-12 py-4 rounded-4xl body-text font-bold`}>{text}</div>
-  )
-}
+    <div
+      className={`${className} cursor-pointer px-12 py-4 rounded-4xl body-text font-bold`}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {text}
+    </div>
+  );
+};
 
-export default Button
+export default Button;
